9eca7f9a844f3fc7c5c89a548b61aedfc92b77e4
Sean Williams <sean@xeed.io>
2026-04-15 15:13:29 -0400

    feat: correct UMA free memory reporting
    Apply vLLM's integrated-GPU memory pattern to AutoRound's\nallocation and offload decisions.\n\nUse psutil available memory on aarch64 CUDA UMA systems,\nand clarify that MemoryMonitor peak_ram is process RSS while\npeak_vram tracks accelerator allocator usage.

--- auto_round/compressors/base.py
+++ auto_round/compressors/base.py
@@ -26,7 +26,7 @@ from typing import Any, Callable, Optional, Union
 import accelerate
 import torch
 from accelerate.big_modeling import dispatch_model, infer_auto_device_map
-from accelerate.utils import get_balanced_memory, get_max_memory
+from accelerate.utils import get_balanced_memory
 from packaging import version
 from torch import autocast
 from tqdm import tqdm
@@ -115,6 +115,7 @@ from auto_round.utils.device import (
     clear_memory_if_reached_threshold,
     dispatch_model_no_offload_aware,
     get_major_device,
+    get_max_memory_with_uma_correction,
     is_single_device_no_offload,
     materialize_model_on_device,
     parse_available_devices,
@@ -2353,7 +2354,7 @@ class BaseCompressor(object):
                         no_split_modules = list(getattr(self.model, "_no_split_modules", []))
                         devices = parse_available_devices(self.device_map)
 
-                        max_memory = get_max_memory()
+                        max_memory = get_max_memory_with_uma_correction()
                         new_max_memory = {}
                         if "cpu" not in devices:
                             devices.append("cpu")
